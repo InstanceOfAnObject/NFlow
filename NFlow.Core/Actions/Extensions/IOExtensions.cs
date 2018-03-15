0fc09e657bbec5644f284bf68e488b854d60d0af
Alexandre Simoes <mail@alexcode.com>
2018-03-16 00:55:15 +0100

    First implementation of FileReaderAction

---
+++ NFlow.Core/Actions/Extensions/IOExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFlow.Core.Actions.Extensions
+{
+    public static class IOExtensions
+    {
+
+        public static FlowActions<T> ReadFile<T>(this FlowActions<T> task, String filePath, FileReaderActionSupportedTypes fileType, String variable)
+        {
+
+            return task.Add(new FileReaderAction<T>(filePath, fileType, variable));
+
+        }
+
+    }
+}
---
+++ NFlow.Core/Actions/FileReaderAction.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NFlow.Core.Actions.Base;
+using NFlow.Core.NotationSupport;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFlow.Core.Actions
+{
+    public class FileReaderAction<T> : BaseFlowAction<T>
+    {
+        String _filePath = null;
+        FileReaderActionSupportedTypes _fileType = FileReaderActionSupportedTypes.JSON;
+        String _variable = null;
+
+        public FileReaderAction(String filepah, FileReaderActionSupportedTypes fileType, String variable)
+        {
+            this._filePath = filepah;
+            this._fileType = fileType;
+            this._variable = variable;
+        }
+
+        public override string Text { get; set; } = "Read file";
+        public override NotationObjectTypes NotationObjectType { get; set; } = NotationObjectTypes.Action;
+        
+        public override void Execute(FlowContext<T> context)
+        {
+            if (this._fileType == FileReaderActionSupportedTypes.JSON)
+            {
+                var obj = ReadJSON(this._filePath);
+                context.Variables.Set(_variable, obj);
+            }
+        }
+
+        private dynamic ReadJSON(String filePath)
+        {
+            dynamic json = JObject.Parse(File.ReadAllText(filePath));
+            return json;
+        }
+
+    }
+
+    /// <summary>
+    /// List of supported file types
+    /// </summary>
+    public enum FileReaderActionSupportedTypes : int
+    {
+        JSON
+    }
+}
